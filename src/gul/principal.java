318557af08dce0141db61b988fec0c1fc9df3fa5
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2019-11-08 16:18:15 -0600

    Add files via upload

---
+++ src/gul/principal.java
@@ -0,0 +1,8 @@
+package gul;
+public class principal {
+    public static void main (String [ ] args) {
+        //aca tendremos todas la inicializaciones de las estructuras
+        //primero llamamos a nuestro jframe de logueo
+        //teniendo ya ingresado nuestru usuario ademin
+    }
+}
